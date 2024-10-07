8adff42f2b7ed71c0cc178f2bb150896a56d0639
Vitor <vitor@helpcorretora.com.br>
2024-10-06 22:51:45 -0300

    primeiro commit do projeto, salvando ele na nuvem

---
+++ app/resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                    
+
+                    @role('admin')
+
+                        Eu sou admin
+                    @else
+                        eu não sou admin
+                    @endrole
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
